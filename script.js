9923baea044024aad22077e6e46df87457353368
Ilona Vasylieva <vasylieva.ilona@gmail.com>
2021-01-27 23:59:42 +0200

    Creating a new repository

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Enter the data to create a formula for calculating the volume of a cylinder");
+
+alert("You chose the base area value equal to " + prompt("What area of the base do you need to set?"));
+
+alert("You selected radius " + prompt("What radius do you want to choose for the formula?"));
+
+alert("You chose the height " + prompt("What height should you choose for the formula?"));
+
+
+
+
