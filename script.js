67e4b9bf60864a49cdcde81dd16710f16d128b3c
Ilona Vasylieva <vasylieva.ilona@gmail.com>
2021-01-30 18:54:48 +0200

    homework

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 alert("Enter the data to create a formula for calculating the volume of a cylinder");
 
-alert("You chose the base area value equal to " + prompt("What area of the base do you need to set?"));
+alert(radius = " " + prompt("What radius do you want to choose for the formula?"));
 
-alert("You selected radius " + prompt("What radius do you want to choose for the formula?"));
+alert(height = " " + prompt("What height should you choose for the formula?"));
 
-alert("You chose the height " + prompt("What height should you choose for the formula?"));
+var test = 3.14159 * (radius * radius) * height;
 
-
-
-
+alert("You chose the base area value equal to " + test);
